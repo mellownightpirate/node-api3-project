aca8d62863edf163a0f81c7ad15cec4d5e68c392
Amin Hasan <amin@Amins-MacBook-Air.local>
2020-03-12 10:12:46 +0000

    mvp

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-const express = require('express');
+const express = require("express");
+const userRouter = require("./users/userRouter");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
-
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(
+    `Time: `,
+    Date.now(),
+    `, Request Type: `,
+    req.method,
+    `, Request URL: `,
+    req.originalUrl
+  );
+  next();
+}
+
+server.use(logger);
+server.use(userRouter);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
 
 module.exports = server;
